82f9cc7291b0c6ffface89b0b4c01e39d08daed0
Ayush Chauhan <101352546+StarBoyHACKER@users.noreply.github.com>
2022-03-26 12:04:20 +0530

    binomial expression

--- binomial.c
+++ binomial.c
@@ -1,61 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
+#include<stdio.h>
 int main()
 {
-float area, radius, s1,s2,s3,base,height,sp;
-int choice;
-printf("1. Area of Circle
-n");
-printf("2. Area of Triangle(Base and Height)
-n");
-printf("3. Area of Triangle(All three Sides)
-n");
-printf("Select your Choice
-n");
-scanf("%
-d",&choice
-switch(choice)
+int i,n,r;
+long int x,y,z,nCr; // factorial of a number may cross the range of the integer,so used long int data type.
+printf("enter the value of n and r:\n");
+scanf("%d %d", &n, &r);
+x=y=z=1;
+//calculate x as factorial n
+for(i=n;i>=1;i--)
 {
-case 1:
-printf("You have chosen Area of Circle
-n");
-printf("Enter the radius
-n");
-scanf("%
-f",&radius
-area=3.14*radius*radius;
-printf("Area of Circle is %.2f
-n",area
-break;
-case 2:
-printf("You have chosen Area of
-Triangle(Base and Height) n");
-printf("Enter base and height
-n");
-scanf("%
-f%f base,&height
-area=(base*height)/2;
-printf("Area of Triangle is
-%.2f n",area
-break;
-  case 3:
-printf("You have chosen Area of Triangle(All three Sides)
-n");
-printf("Enter values of all three sides
-n");
-scanf("%f%f%f",&s1,&s2,&s3);
-sp
-=(s1+s2+s3)/
-area=sqrt(
-sp *(sp s1)*(sp s2)*(sp s3));
-printf("Area of Triangle is %.2f
-n",area
-break;
-default:
-printf("Sorry, Invalid Choice
-n");
+x=x*i;
 }
-printf("Thank You
-n");
+//calculate y as factorial (n r)
+for(i=n-r;i>=1;i--)
+{
+y=y*i;
+}
+//calculate z as factorial r
+for(i=r;i>=1;i--)
+{
+z=z*i;
+}
+//Using x,y,z calculate binomial coefficient nCr
+nCr=x/(y*z);
+printf("%dC%d=%d",n,r,nCr);
 return 0;
+}
