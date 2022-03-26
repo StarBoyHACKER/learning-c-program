3d3853d13f511f45b8d9ce44039b1bc1b60e8772
Ayush Chauhan <101352546+StarBoyHACKER@users.noreply.github.com>
2022-03-26 12:13:15 +0530

    finding odd even numbers

---
+++ oddeven.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+int
+num =
+printf("enter the number");
+scanf("%d",&
+num
+if(n%2==0)
+{
+printf("%d number in even",
+num
+}
+else
+{
+printf("%d number in odd",
+num
+}
+}
